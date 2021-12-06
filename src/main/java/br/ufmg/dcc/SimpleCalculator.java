bb398863f988f1ec563948f35004678d2cf1f2af
John DiTaranto <jjditaranto@loyola.edu>
2021-12-06 15:42:41 -0500

    changed add function

--- src/main/java/br/ufmg/dcc/SimpleCalculator.java
+++ src/main/java/br/ufmg/dcc/SimpleCalculator.java
@@ -3,7 +3,7 @@ package br.ufmg.dcc;
 public class SimpleCalculator {
 
   public int addition(int x, int y) {
-    return x + y - 7;
+    return x + y - 7 + 2;
   }
 
   public int subtraction(int x, int y) {
